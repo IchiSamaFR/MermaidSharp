f6f66e61116719df4b6d1fd7042f1a6d6af10dfd
IchiSamaFR <djordan-dsousa@hotmail.fr>
2026-03-22 12:45:49 +0100

    tests: entity framwork diagram test for generic class 4.8

---
+++ src/MermaidSharp.EntityFramework.Tests/Mocks/Entities/IdentityRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MermaidSharp.EntityFramework.Tests.Mocks.Entities
+{
+    internal class IdentityRole<T>
+    {
+        public T Id { get; set; }
+        public string Name { get; set; }
+    }
+}
