ed82cc44b0335278a10609346864495302a25043
jordan de sousa <djordan-dsousa@hotmail.fr>
2026-03-25 10:39:26 +0100

    tests: add unit tests for GitGraph, GitBranch, GitCheckout, GitCommit, GitMerge, and GitGraphConfig

---
+++ src/MermaidSharp.Tests/GitGraphs/GitCheckoutTests.cs
@@ -0,0 +1,30 @@
+﻿using MermaidSharp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MermaidSharp.Tests.GitGraphs
+{
+    [TestClass]
+    public class GitCheckoutTests
+    {
+        [TestMethod]
+        public void GitCheckout()
+        {
+            //Arrange
+            var checkout = new GitCheckout("myBranch");
+            string expected = @"checkout myBranch";
+
+            //Act
+            string result = checkout.ToString();
+
+            //Assert
+            Assert.IsNotNull(checkout);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ src/MermaidSharp.Tests/GitGraphs/GitCommitTests.cs
@@ -0,0 +1,79 @@
+﻿using MermaidSharp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MermaidSharp.Tests.GitGraphs
+{
+    [TestClass]
+    public class GitCommitTests
+    {
+        [TestMethod]
+        public void GitCommit()
+        {
+            //Arrange
+            var commit = new GitCommit();
+            string expected = @"commit";
+
+            //Act
+            string result = commit.ToString();
+
+            //Assert
+            Assert.IsNotNull(commit);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GitCommit_Id()
+        {
+            //Arrange
+            var commit = new GitCommit("my commit desc");
+            string expected = @"commit id: ""my commit desc""";
+
+            //Act
+            string result = commit.ToString();
+
+            //Assert
+            Assert.IsNotNull(commit);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GitCommit_Tag()
+        {
+            //Arrange
+            var commit = new GitCommit(tag: "my tag");
+            string expected = @"commit tag: ""my tag""";
+
+            //Act
+            string result = commit.ToString();
+
+            //Assert
+            Assert.IsNotNull(commit);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void GitCommit_IdAndTag()
+        {
+            //Arrange
+            var commit = new GitCommit("my commit desc", "my tag");
+            string expected = @"commit id: ""my commit desc"" tag: ""my tag""";
+
+            //Act
+            string result = commit.ToString();
+
+            //Assert
+            Assert.IsNotNull(commit);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
